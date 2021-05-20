434c37271cd32a147d83905c36d7c1b1c66719cf
argmnt <yagizgazibaba1996@hotmail.com>
2021-05-21 01:45:55 +0300

    service registration added.

--- target/generated-sources/annotations/guru/springframework/msscbeerservice/web/mappers/BeerMapperImpl.java
+++ target/generated-sources/annotations/guru/springframework/msscbeerservice/web/mappers/BeerMapperImpl.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-05-04T14:41:07+0300",
+    date = "2021-05-19T15:29:48+0300",
     comments = "version: 1.3.0.Final, compiler: javac, environment: Java 11.0.10 (AdoptOpenJDK)"
 )
 @Component
